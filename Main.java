672089100defde6641fdd6f04ac819b686b04a5e
LightningMonster <lightningmonster.1548@gmail.com>
2024-08-04 21:46:36 +0530

    Printing numbers

--- Main.java
+++ Main.java
@@ -4,5 +4,10 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        System.out.println(5);
+        System.out.println(50);
+        System.out.println(500);
+        System.out.println(5+5);
     }
 }
