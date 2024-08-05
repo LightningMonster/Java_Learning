154c0d571a9a505e0bfcea318ccdf398c7a5b9f7
LightningMonster <lightningmonster.1548@gmail.com>
2024-08-05 21:55:22 +0530

    Operators

---
+++ Day2.java
@@ -0,0 +1,31 @@
+//Day2
+public class Day2 {
+    public static void main(String[] args){
+        //Arithmatic Operators
+
+        int sum = 5 + 5;
+        System.out.println("Addition 5+5: "+sum);
+
+        int sub = 5 - 5;
+        System.out.println("Subtraction 5-5: "+sub);
+
+        int mul = 5 * 5;
+        System.out.println("Multiplication 5*5: "+mul);
+
+        int div = 5 / 5;
+        System.out.println("Division 5/5: "+div);
+
+        int mod = 16 % 5;
+        System.out.println("Addition 16%5: "+mod);
+
+        int inc = 0;
+        System.out.println("\nBefore increment: "+inc);
+        inc++;
+        System.out.println("After increment: "+inc);
+        
+        int dec = 10;
+        System.out.println("\nBefore decrement: "+dec);
+        dec++;
+        System.out.println("After decrement: "+dec);
+    }
+}
