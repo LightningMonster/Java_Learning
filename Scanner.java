b0a9a15bbbeeedb6d8f2ea4ea3b1a1ffec4afc82
Yash Dhadve <lightningmonster.1548@gmail.com>
2024-10-02 19:42:42 +0530

    Creating Package

--- Scanner.java
+++ Scanner.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
+import myPack.MyPackageClass;  // Import the user-defined package
 
 class ScannerTest {
     public static void main(String[] args) {
-
         Scanner s = new Scanner(System.in);
         System.out.println("Enter username: ");
 
         String userName = s.nextLine();
-        System.out.println("Hii "+userName);
+        System.out.println("Hi " + userName);
+
+        // Calling the method from MyPackageClass
+        MyPackageClass.printMessage();  
+
         s.close();
     }    
 }
---
+++ myPack/MyPackageClass.java
@@ -0,0 +1,7 @@
+package myPack;
+
+public class MyPackageClass {
+    public static void printMessage() {
+        System.out.println("This is my package!");
+    }
+}
